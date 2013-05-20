1c5eb8a8a0d967a992725790444991f4a80f3ee6
dylan_wei <dylan.wq@gmail.com>
2013-05-20 21:06:33 +0800

    add weather winD

--- wind-arrow.js
+++ wind-arrow.js
@@ -1,1 +1,24 @@
 // js for weather
+function drawWeatherPic(ctxObj,x,y,winD,winR){
+    var lineW = 30;//风向线的长度
+    
+	if(winD == null ||winD == 'undefined' ){return;}
+	//if(winR == null ||winR == 'undefined' ){return;}
+	if (canvas.getContext){
+		var ctx = ctxObj.getContext('2d');
+		//画点
+		ctx.beginPath();
+		ctx.arc(x,y,2,0,Math.PI*2,true);
+		ctx.stroke();
+		
+		//画风向
+		ctx.beginPath();
+		ctx.moveTo(x,y);
+		var left = x+Math.sin(wd*(Math.PI*2/360))*lineW;
+		var top = y-Math.cos(wd*(Math.PI*2/360))*lineW;
+		ctx.lineTo(left,top);
+		ctx.stroke(); 
+  }else{
+ 
+ }
+
